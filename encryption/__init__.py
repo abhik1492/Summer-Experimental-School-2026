39c1cfdb7e576908c1943c903cf8678d03c8813c
ab1492 <ab1876@exeter.ac.uk>
2026-06-23 16:13:17 +0100

    setup skeleton for 2 letter word task

---
+++ encryption/__init__.py
@@ -0,0 +1,73 @@
+from otree.api import *
+
+
+doc = """
+Encryption task
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'encryption'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 3
+    PAYMENT_PER_CORRECT = 0.10
+
+
+class Subsession(BaseSubsession):
+    payment_per_correct = models.CurrencyField()
+    lookup_table = models.StringField
+    word = models.StringField()
+
+    def setup_round(self):
+        self.payment_per_correct = Currency(C.PAYMENT_PER_CORRECT)
+        self.word = "AB"
+
+    @property
+    def lookup_dict(self):
+        return{
+            "A": 1,
+            "B": 2,
+        }
+
+def creating_session(subsession):
+    subsession.setup_round()
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    time_for_task = models.IntegerField()
+    response_1 = models.IntegerField()
+    response_2 = models.IntegerField()
+    response_3 = models.IntegerField()
+    response_4 = models.IntegerField()
+    response_5 = models.IntegerField()
+    is_correct = models.BooleanField()
+
+
+# PAGES
+class Intro(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player_round_number == 1
+
+class Decision(Page):
+    form_model = 'player'
+    form_fields = ['response_1',
+                   'response_2',
+                   ]
+
+
+class Results(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player_round_number == C.NUM_ROUNDS
+
+
+page_sequence = [
+    Intro,
+    Decision,
+    Results
+]
